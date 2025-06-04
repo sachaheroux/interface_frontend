31ccf649aad563c495fb650931067b3981cda05e
Sacha Héroux <sacha.heroux87@gmail.com>
2025-06-04 11:46:34 -0400

    jbfjflk

--- src/components/FMSSacADosGloutonForm.jsx
+++ src/components/FMSSacADosGloutonForm.jsx
@@ -124,7 +124,7 @@ export default function FMSSacADosGloutonForm() {
       
       console.log("Données envoyées au backend:", donnees);
       
-      const response = await fetch("http://localhost:8000/fms/sac_a_dos_glouton", {
+      const response = await fetch("http://127.0.0.1:8000/fms/sac_a_dos_glouton", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(donnees)
